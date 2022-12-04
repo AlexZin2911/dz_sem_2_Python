7bee37bbc889a8f48ce160efb194066325bfcb36
Alex <leschazin4encko@yandex.ru>
2022-12-04 15:20:09 +0400

    4

--- task_3.py
+++ task_3.py
@@ -1,43 +1,33 @@
-# # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
-# # Найдите произведение элементов на указанных позициях. 
+# Задайте список из (2*N+1) элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных ИНДЕКСАХ. Пять ИНДЕКСОВ хранятся в списке, который вы сами заполняете.
 
-# from random import randint
-# list = []
+# Пример списка ИНДЕКСОВ [2, 2, 3, 1, 8]
 
-# print(' Введите натуральное положительное число')
-# n = int(input())
-# for i in range(n):
-#     list(-n,n)
-# print(list)
-
-# def get_list(list):
-#     count =0 
-#     for element in list:
-#         count +=1
-#     return count
-# print('Колличество элементов: ', get_list(list))
-
-# x = int(input('Введите первый номер: '))
-# y = int(input('Введите второй номер: '))
-# z = int(input('Введите третий номер: '))
-# m = int(input('Введите четвертый номер: '))
-# d = int(input('Введите пятый номер: '))
-
-# for i in range(len(list)):
-#     multiplication = list[x -1] * list[y - 1] * list[z - 1] * list[m - 1] * list[d - 1]
-# print(f'Произведдение чисел на указанных местах равно: {list[x -1]} * {list[y -1]} * {list[z - 1]} * {list[m - 1]} * {list[d - 1]} =', multiplication)
+# Ввод: 4
+# [-4, -3, -2, -1, 0, 1, 2, 3,4]
 
 from random import randint
+list = []
 
-def list(n):
-    list = []
-    for i in range(n):
-        list.append(randint(-n, n))
-    return list
+print(' Введите натуральное положительное число')
+n = int(input())
+for i in range(n):
+    list.append(randint (-n,n))
+print(list)
 
-n = int(input('Введите число N: '))
-numbers = list(n)
-print(numbers)
-x = list(map(int, input("Введите числа через пробел:\n").split()))
-result = numbers[int(x.readline())] * numbers[int(x.readline(2))]
-print(result)
+def get_list(list):
+    count =0 
+    for element in list:
+        count +=1
+    return count
+print('Колличество элементов: ', get_list(list))
+
+x = int(input('Введите первый номер: '))
+y = int(input('Введите второй номер: '))
+z = int(input('Введите третий номер: '))
+m = int(input('Введите четвертый номер: '))
+d = int(input('Введите пятый номер: '))
+
+for i in range(len(list)):
+    multiplication = list[x -1] * list[y - 1] * list[z - 1] * list[m - 1] * list[d - 1]
+print(f'Произведдение чисел на указанных местах равно: {list[x -1]} * {list[y -1]} * {list[z - 1]} * {list[m - 1]} * {list[d - 1]} =', multiplication)
